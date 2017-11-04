7c306c2b9e1969fb543b7af0271d375591de8fc9
EastmanJian <50935@qq.com>
2017-11-04 15:37:11 +0800

    add react demos

--- react-demo-app/src/App.js
+++ react-demo-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import InitApp from './InitApp'
 import RouterBasic from './RouterBasic'
+import JsxJsExpression from './jsx_js_expression'
+
 import {
     BrowserRouter as Router,
     Route,
@@ -14,6 +16,7 @@ const DemoRouter = () => (
             <Route exact path="/react_demo" component={Home}/>
             <Route path="/react_demo/init_app" component={InitApp}/>
             <Route path="/react_demo/router_basic" component={RouterBasic}/>
+            <Route path="/react_demo/jsx_js_expression" component={JsxJsExpression}/>
         </div>
     </Router>
 )
@@ -22,9 +25,10 @@ const Home = () => (
     <div>
         <h2>Demo Home</h2>
         <ul>
-            <li><Link to="./">Demo Home</Link></li>
-            <li><Link to="./init_app">Init App</Link></li>
-            <li><Link to="./router_basic">Basic Router</Link></li>
+            <li><Link to="/react_demo/">Demo Home</Link></li>
+            <li><Link to="/react_demo/init_app">Init App</Link></li>
+            <li><Link to="/react_demo/router_basic">Basic Router</Link></li>
+            <li><Link to="/react_demo/jsx_js_expression">JSX JavaScript Expression in curly braces</Link></li>
         </ul>
     </div>
 )
--- react-demo-app/src/index.js
+++ react-demo-app/src/index.js
@@ -4,5 +4,26 @@ import './index.css';
 import DemoRouter from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+const element = <h5>This is an index page of React JS demos</h5>;
+
+
+function tick() {
+    const element = (
+        <div>
+            <h3>Hello, world!</h3>
+            <h4>It is {new Date().toLocaleTimeString()}.</h4>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('clock')
+    );
+}
+
+setInterval(tick, 1000);
+
 ReactDOM.render(<DemoRouter />, document.getElementById('root'));
+ReactDOM.render(element, document.getElementById('desc'));
+
 registerServiceWorker();
